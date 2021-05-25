bd846b3609a9e8ecc25ef1498a8e43fe1bb7ef93
Ondrej Maksi <ondrej.maksi@gmail.com>
2021-05-25 11:18:09 +0200

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,9 @@ module.exports = {
   extends: ['react-app', 'prettier'],
   plugins: ['prettier'],
   rules: {
+    'no-var': 'warn',
+    'prefer-const': 'warn',
+    'no-console': 'warn',
     'prettier/prettier': 'warn',
     'arrow-body-style': 'off',
     'prefer-arrow-callback': 'off',
