1b62452fc0d546f6df22f01bca73fb07fd7dff27
Ondrej Maksi <ondrej.maksi@gmail.com>
2021-05-25 11:18:49 +0200

    Update eslint.config-jest.js

--- eslint.config-jest.js
+++ eslint.config-jest.js
@@ -1,6 +1,13 @@
 module.exports = {
   extends: ['react-app', 'react-app/jest', 'prettier'],
-  rules: {},
+  rules: {
+    'no-var': 'warn',
+    'prefer-const': 'warn',
+    'no-console': 'warn',
+    'prettier/prettier': 'warn',
+    'arrow-body-style': 'off',
+    'prefer-arrow-callback': 'off',
+  },
   overrides: [
     {
       files: ['**/*.ts?(x)'],
